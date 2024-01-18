e23d14c6ad945f98944a85e3216ac4dac8b83b42
ucup-dev <iyanpratama2002@gmail.com>
2024-01-18 19:40:03 +0700

    set basic auth to standard

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -8,16 +8,21 @@ function authentication(req, res, next) {
         err.status = 401;
         return next(err)
     }
-
-    const splittedAuth = authheader.split(" ")
  
-    // console.log("AUTH", splittedAuth[1])
+    const auth = new Buffer.from(authheader.split(' ')[1],
+        'base64').toString().split(':');
+    const user = auth[0];
+    const pass = auth[1];
  
-    if (splittedAuth[1] == 'ucup') {
+    if (user == 'admin' && pass == 'password') {
+ 
         // If Authorized user
         next();
     } else {
-        next(new Error('You are not authenticated!'));
+        let err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic');
+        err.status = 401;
+        return next(err);
     }
  
 }
